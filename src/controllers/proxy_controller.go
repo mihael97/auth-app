ba144ad0fd78a6cc24d9b31131578d9fc038d05f
Mihael Macuka <mihaelmacuka2@gmail.com>
2023-04-08 20:22:35 +0200

    #11 Username and role header

--- src/controllers/proxy_controller.go
+++ src/controllers/proxy_controller.go
@@ -9,16 +9,22 @@ import (
 	"strings"
 
 	"github.com/gin-gonic/gin"
+	"github.com/mihael97/auth-proxy/src/services"
 	"github.com/mihael97/auth-proxy/src/util"
 	exceptionUtil "gitlab.com/mihael97/Go-utility/src/util"
 	"gitlab.com/mihael97/Go-utility/src/web"
 	"gitlab.com/mihael97/Go-utility/src/web/routes"
+	"gitlab.com/mihael97/Go-utility/src/web/security/jwt"
 )
+
+const UsernameHeader = "X-MACUKA-USERNAME"
+const RolesHeader = "X-MACUKA-ROLES"
 
 var proxyControllerImpl *proxyController
 
 type proxyController struct {
 	routingTable map[string]*gin.Engine
+	userService  services.UserService
 }
 
 func (*proxyController) GetBasePath() string {
@@ -47,7 +53,8 @@ func (p *proxyController) getRemoteUrl(ctx *gin.Context) (*url.URL, bool, error)
 		log.Printf("URL %s not found\n", appName)
 		return nil, false, nil
 	}
-	newUrl := fmt.Sprintf("http://localhost:%d%s", port, path)
+	path = strings.ReplaceAll(path, "/"+appName, "")
+	newUrl := fmt.Sprintf("http://localhost:%d%s/", port, path)
 	log.Println("New url is ", newUrl)
 	newPath, err := url.Parse(newUrl)
 	return newPath, true, err
@@ -74,6 +81,11 @@ func (p *proxyController) proxyRequests(ctx *gin.Context) {
 	}
 
 	proxy := httputil.NewSingleHostReverseProxy(remote)
+	modifyHeadersStatus := p.modifyHeaders(ctx)
+	if !modifyHeadersStatus {
+		web.WriteErrorMessage("error during modifiying headers", ctx)
+		return
+	}
 	proxy.Director = func(req *http.Request) {
 		req.Header = ctx.Request.Header
 		req.Host = remote.Host
@@ -83,15 +95,35 @@ func (p *proxyController) proxyRequests(ctx *gin.Context) {
 	}
 
 	proxy.ServeHTTP(ctx.Writer, ctx.Request)
+	p.modifyHeaders(ctx, true)
 	if ctx.Writer.Status() == http.StatusBadGateway {
 		web.WriteErrorMessage("error during proxying", ctx)
 	}
+}
+
+// Add username and roles header
+func (p *proxyController) modifyHeaders(ctx *gin.Context, remove ...bool) bool {
+	if len(remove) != 0 && remove[0] {
+		ctx.Writer.Header().Del(UsernameHeader)
+		ctx.Writer.Header().Del(RolesHeader)
+	} else {
+		username, err := jwt.GetUserNameFromToken(ctx, *util.GetConfig().Security.Secret)
+		userData, _ := p.userService.GetUser(username)
+		if err != nil {
+			log.Println("Error during parsing token", err)
+			return false
+		}
+		ctx.Request.Header.Add(UsernameHeader, username)
+		ctx.Request.Header.Add(RolesHeader, strings.Join(userData.Roles, ","))
+	}
+	return true
 }
 
 func GetProxyController() routes.RoutesController {
 	if proxyControllerImpl == nil {
 		proxyControllerImpl = &proxyController{
 			map[string]*gin.Engine{"/api/login": GetLoginController(), "/api/users": GetUserController()},
+			services.GetUserService(),
 		}
 	}
 	return proxyControllerImpl
