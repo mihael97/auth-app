31290c36dba5cc1cfddc0c1b0d559ea5dc79b771
mmacuka <mihaelmacuka2@gmail.com>
2023-08-27 18:16:26 +0200

    [#42] Sentry init

--- src/controllers/controller_util.go
+++ src/controllers/controller_util.go
@@ -2,12 +2,15 @@ package controllers
 
 import (
 	"fmt"
+	"github.com/getsentry/sentry-go"
+	sentrygin "github.com/getsentry/sentry-go/gin"
 	"github.com/gin-gonic/gin"
 	"github.com/mihael97/auth-proxy/docs"
 	"github.com/mihael97/auth-proxy/src/security"
 	config "github.com/mihael97/auth-proxy/src/util"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
+	"gitlab.com/mihael97/Go-utility/src/env"
 	"gitlab.com/mihael97/Go-utility/src/web"
 	middlewares "gitlab.com/mihael97/Go-utility/src/web/middlewares/security"
 	"gitlab.com/mihael97/Go-utility/src/web/routes"
@@ -24,6 +27,7 @@ func InitializeRoutes(engine *gin.Engine) {
 
 	engine.Use(middlewares.CORSMiddleware())
 	engine.Use(security.JwtMiddleware())
+	setupSentry(engine)
 
 	log.Print("Adding controller routes")
 	routes.AddControllerRoutesWithFilter(false, engine, controllers)
@@ -63,4 +67,22 @@ func appendExpiresAt(ctx *gin.Context) {
 	if remove {
 		ctx.Request.Header.Del(security.AuthorizationHeader)
 	}
+}
+func setupSentry(engine *gin.Engine) {
+	dsn := env.GetEnvVariable("SENTRY_DSN", "")
+	if len(dsn) == 0 {
+		return
+	}
+	err := sentry.Init(sentry.ClientOptions{
+		Dsn:              dsn,
+		EnableTracing:    true,
+		TracesSampleRate: 1.0,
+		Environment:      env.GetEnvVariable("SENTRY_ENVIRONMENT", "local"),
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	engine.Use(sentrygin.New(sentrygin.Options{
+		Repanic: true,
+	}))
 }
